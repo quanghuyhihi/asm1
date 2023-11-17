916ff0015d6b9f350656811ce8736b4e3abab075
quanghuyhihi <huypqph30308@fpt.edu.vn>
2023-11-17 18:38:34 +0700

    zo

---
+++ ASM1/src/pages/ProfilePage.tsx
@@ -0,0 +1,20 @@
+import "../App.css"
+const ProfilePage = () => {
+    return <div><h2>ProfilePage</h2>
+      <div className="container">
+  <div className="shape">
+    <div className="image" />
+  </div>
+  <h3>Phạm Quang Huy</h3>
+  <h3 className="title">Web Designer</h3>
+  <h3>23 years old</h3>
+  <h3>Ha Noi</h3>
+  <h3>C,PHP,JS,...</h3>
+  
+</div>
+
+    </div>;
+  };
+  
+  export default ProfilePage;
+  
